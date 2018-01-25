9b7dfd40a44d23048c2ca9c4d4d3082666cd7f44
Glavene <35814759+Glavene@users.noreply.github.com>
2018-01-25 22:50:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,15 @@ app.listen(server_port, server_ip_address, function () {
     console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
 
 });
-	
+
+app.get('/',function(req,res){
+	var dane = {
+		"dane":""
+	};
+	dane["dane"] = ": Odczyt: /getdata Dodaj: /writedata";
+	res.json(dane);
+});	
+
 app.get('/getdata',function(req,res){
 	var dane = {
 		"dane":""

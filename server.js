08383b8384fdb64a13a4f5830bc7e8fe2ebb9f01
Glavene <35814759+Glavene@users.noreply.github.com>
2018-01-25 22:27:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(server_port, server_ip_address, function () {
+
+    console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
+
+});
+
+
 var app   = require('express')();
 var http = require('http').Server(app);
 var mysql = require('mysql');

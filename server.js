1fa884ea2c1f6a82fadf0b1ab23f0793f62d1970
Glavene <35814759+Glavene@users.noreply.github.com>
2018-02-01 23:13:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-
 
 var app   = require('express')();
 var http = require('http').Server(app);
